486dbfe50f720ccbb604b2a60626b3edfba9ca19
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-12-22 01:55:21 +0600

    hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
@@ -12,9 +13,11 @@ function App() {
         <Header></Header>
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="orders" element={<OrderReview />} />
-          <Route path="inventory" element={<Inventory />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/orders" element={<OrderReview />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/*" element={<NotFound />} />
+
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useCart from '../../Hooks/useCart';
+import useProducts from '../../Hooks/useProducts';
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
-            <h2>This is Order Review</h2>
+            <h2>Total number of product:{products.length}</h2>
+            <h2>Cart product: {cart.length}</h2>
         </div>
     );
 };
