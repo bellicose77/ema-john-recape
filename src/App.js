dc810bc49fef9048272c8d932b01a6ea8a8e3e96
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-12-22 00:49:01 +0600

    link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import OrderReview from './components/OrderReview/OrderReview';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="orders" element={<OrderReview />} />
+          <Route path="inventory" element={<Inventory />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>This is NOt found page. 404 errror </h2>
+        </div>
+    );
+};
+
+export default NotFound;
