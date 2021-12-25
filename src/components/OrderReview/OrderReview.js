b8567788a20f17e0fab69f40f60f5aada52ecb46
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-12-25 21:22:56 +0600

    final

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useCart from '../../Hooks/useCart';
 import useProducts from '../../Hooks/useProducts';
 import { clearTheCart, deleteFromDb, getStoredCart } from '../../utilities/fakedb';
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -37,7 +37,18 @@ const Shop = () => {
     }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        const exits = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exits) {
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exits.quantity = exits.quantity + 1;
+            newCart = [...rest, product]
+
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
         setCart(newCart);
         // save to local storage (for now)
         addToDb(product.key);
